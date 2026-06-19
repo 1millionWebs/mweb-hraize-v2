f19f4651ebeaa6dda2e89db8b6a16f22163b7df2
Beeshub123 <hariharan883884@gmail.com>
2026-06-19 13:45:22 +0530

    Install wrangler

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  skipTrailingSlashRedirect: true,
+};
 
 export default nextConfig;
